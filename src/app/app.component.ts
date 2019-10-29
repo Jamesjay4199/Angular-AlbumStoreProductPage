6ea16ee87262ad5d5a731bd677e44ae7e532e387
James James <jamesjay4199@gmail.com>
2019-10-29 13:57:42 +0100

    part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../app/product.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'app works!';
